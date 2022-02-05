33ae5ae1ecb6dc8529ff3e841a91398146547cdf
James Liang <86000097+jamesliangg@users.noreply.github.com>
2022-02-05 15:15:48 -0500

    Fixed div by 0
    Added one to dislike so wouldn't divide by 0

--- Main.java
+++ Main.java
@@ -18,18 +18,18 @@ class Main{
         videoInfo = Dislikes.getRatio(rawVideo);
         int[] ratios = new int[5]; 
         for (int i = 0; i < rawVideo.size(); i++){
-            ratios[i] = videoInfo.get(i).getLikes()/videoInfo.get(i).getDislikes();
+            ratios[i] = videoInfo.get(i).getLikes()/(videoInfo.get(i).getDislikes() + 1);
         }
         Arrays.sort(ratios);
         for (int temp = 4; temp >= 0; temp--){
             for (int i = 0; i < ratios.length; i++){
-                if (ratios[temp] == videoInfo.get(i).getLikes()/videoInfo.get(i).getDislikes()){
+                if (ratios[temp] == videoInfo.get(i).getLikes()/(videoInfo.get(i).getDislikes() + 1)){
                     System.out.println(rawVideo.get(i).getTitle());
                     System.out.println("URL:      " + "https://www.youtube.com/watch?v=" + videoInfo.get(i).getID());
                     System.out.println("Views:    " + videoInfo.get(i).getViewCount());
                     System.out.println("Likes:    " + videoInfo.get(i).getLikes());
                     System.out.println("Dislikes: " + videoInfo.get(i).getDislikes());
-                    System.out.println("Ratio:    " + videoInfo.get(i).getLikes()/videoInfo.get(i).getDislikes());
+                    System.out.println("Ratio:    " + videoInfo.get(i).getLikes()/(videoInfo.get(i).getDislikes() + 1));
                     System.out.println();
                 }
             }
